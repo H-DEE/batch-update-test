d1c01126bab566ae3ca92a004800e0eff4cd4606
H.D <harshdeep804@gmail.com>
2016-07-20 18:33:28 +0530

    Updated data

--- update_data.sh
+++ update_data.sh
@@ -1,14 +1,16 @@
 # @Author: H.D.
 # @Date:   2016-07-20 17:47:44
 # @Last Modified by:   H.D.
-# @Last Modified time: 2016-07-20 18:25:39
+# @Last Modified time: 2016-07-20 18:33:00
 
 #!/bin/sh
 git add -A
 git commit -m "Updated data"
 git pull origin master
-git branch -d -r gh-pages
-git checkout -b gh-pages
 git push origin master
+git branch -D gh-pages
+git push origin :gh-pages
+git branch gh-pages
+git push origin gh-pages
 echo Press Enter to close this window...
 read
