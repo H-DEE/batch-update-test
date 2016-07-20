c3256faa53d146dca56db87f427733ec85ce26c4
H.D <harshdeep804@gmail.com>
2016-07-20 17:51:19 +0530

    made changes

---
+++ update_data.sh
@@ -0,0 +1,12 @@
+# @Author: H.D.
+# @Date:   2016-07-20 17:47:44
+# @Last Modified by:   H.D.
+# @Last Modified time: 2016-07-20 17:51:08
+
+#!/bin/sh
+git add -A
+git commit -m "made changes"
+git pull origin master
+git push origin master
+echo Press Enter...
+read
