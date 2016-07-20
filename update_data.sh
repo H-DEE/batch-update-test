6843cb4aa96463bce14f3bf51b4bccda8259fba0
H.D <harshdeep804@gmail.com>
2016-07-20 17:52:26 +0530

    made changes

--- update_data.sh
+++ update_data.sh
@@ -1,12 +1,12 @@
 # @Author: H.D.
 # @Date:   2016-07-20 17:47:44
 # @Last Modified by:   H.D.
-# @Last Modified time: 2016-07-20 17:51:08
+# @Last Modified time: 2016-07-20 17:52:09
 
 #!/bin/sh
 git add -A
 git commit -m "made changes"
 git pull origin master
 git push origin master
-echo Press Enter...
+echo Press Enter to close this window...
 read
