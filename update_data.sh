7d349fba5aafad5528b785511ad5772baee71274
H.D <harshdeep804@gmail.com>
2016-07-20 18:41:53 +0530

    Updated data

--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,7 @@
 # @Author: H.D.
 # @Date:   2016-07-20 17:47:44
 # @Last Modified by:   H.D.
-# @Last Modified time: 2016-07-20 18:33:00
+# @Last Modified time: 2016-07-20 18:41:31
 
 #!/bin/sh
 git add -A
@@ -12,5 +12,5 @@ git branch -D gh-pages
 git push origin :gh-pages
 git branch gh-pages
 git push origin gh-pages
-echo Press Enter to close this window...
+echo "Task Complete! Press Enter to close this window..."
 read
